ad09fb4964c62bc66b9bc3980a1f3c3dcc476735
Gunak00 <pkonwa00@gmail.com>
2023-05-26 00:00:21 +0200

    Info about booked car is displaying.

--- src/app/components/reservation/reservation.component.ts
+++ src/app/components/reservation/reservation.component.ts
@@ -1,9 +1,13 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 import {Car} from "../car/model/car";
 import {CarService} from "../car/service/car.service";
 import {ActivatedRoute} from "@angular/router";
 import {FormBuilder, FormGroup} from "@angular/forms";
 import * as moment from 'moment';
+import {CarFuelType} from "../car/enums/carFuelType";
+import {CarDriveType} from "../car/enums/carDriveType";
+import {CarGearboxType} from "../car/enums/carGearboxType";
+import {DomSanitizer, SafeUrl} from "@angular/platform-browser";
 
 
 @Component({
@@ -24,8 +28,10 @@ export class ReservationComponent implements OnInit{
   public endDate: string;
   public pickUpLocations: string[] = ['Lublin, ul. Kowalska 15', "Lublin, ul. Brzozowa 2", "Lublin, al. Solidarności 75"];
   public returnLocations: string[] = this.pickUpLocations;
+  public defaultImage: string = "../../../assets/images/home-page/small.jpg";
 
-  constructor(private carService: CarService, private route: ActivatedRoute, private formBuilder: FormBuilder) {
+  constructor(private carService: CarService, private route: ActivatedRoute, private formBuilder: FormBuilder,
+              private sanitizer: DomSanitizer) {
     this.formGroup = this.formBuilder.group({
       pickUpLocation: [''],
       returnLocation: [''],
@@ -41,8 +47,10 @@ export class ReservationComponent implements OnInit{
 
     this.carService.getCar(this.carId).subscribe(car => {
       this.car = car;
+      this.getCarImage(this.car);
       console.log(this.car);
     });
+
 
     this.formGroup.get('dateRangeStart').valueChanges.subscribe(start => {
       this.startDate = start;
@@ -55,11 +63,40 @@ export class ReservationComponent implements OnInit{
         this.price = this.calculatePrice(this.numberOfDays);
       }
     })
-
   }
 
   public submitForm(){
     console.log(this.formGroup.value);
+  }
+
+  public getReadableValueOfFuel(carFuelType: CarFuelType) {
+    return this.carService.getReadableValueOfFuel(carFuelType);
+  }
+
+  public getReadableValueOfDrive(carDriveType: CarDriveType) {
+    return this.carService.getReadableValueOfDrive(carDriveType);
+  }
+
+  public getReadableValueOfGearbox(carGearboxType: CarGearboxType) {
+    return this.carService.getReadableValueOfGearbox(carGearboxType);
+  }
+
+  public getReadableValueOfCategories(category: string) {
+    return this.carService.getReadableValueOfCategories(category);
+  }
+
+  private getCarImage(car: Car) {
+    const urlCreator = window.URL;
+    if (car.imageUrl == null){
+      car.imageSafeUrl = this.defaultImage;
+    }else{
+      let imageUrl: SafeUrl;
+      this.carService.getImage(car.id, car.imageUrl).subscribe(blob => {
+        imageUrl = this.sanitizer.bypassSecurityTrustUrl(
+          urlCreator.createObjectURL(blob));
+        car.imageSafeUrl = imageUrl;
+      })
+    }
   }
 
   private calculatePrice(days: number){
